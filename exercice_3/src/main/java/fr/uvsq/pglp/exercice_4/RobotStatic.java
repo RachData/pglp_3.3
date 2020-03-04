cb9f651a5ae099aecf6f9c045be542ff7a8bb205
RachData <rachidderra@gmail.com>
2020-03-04 01:27:58 +0100

    Creation de la classe Robot Static

---
+++ exercice_3/src/main/java/fr/uvsq/pglp/exercice_4/RobotStatic.java
@@ -0,0 +1,15 @@
+package fr.uvsq.pglp.exercice_4;
+
+public class RobotStatic {
+	
+	public RobotStatic() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void avance() {
+		throw new UnsupportedOperationException();
+	}
+
+
+}
