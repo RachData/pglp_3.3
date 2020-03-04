6de736fed9a92e3f58205b81d5ca94df2d5e50e0
RachData <rachidderra@gmail.com>
2020-03-04 01:23:39 +0100

    Creation de la classe Robot type 2

---
+++ exercice_3/src/main/java/fr/uvsq/pglp/exercice_4/Robot2.java
@@ -0,0 +1,15 @@
+package fr.uvsq.pglp.exercice_4;
+
+public class Robot2 extends Robot{
+	
+	public Robot2(int position) {
+		super(position);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void avance() {
+		super.position+=20;
+	}
+
+
+}
